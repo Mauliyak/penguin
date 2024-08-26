cec28d4b2e61c499c716354b8bc308ad75d2a05e
Mauliyak <148433945+Mauliyak@users.noreply.github.com>
2024-08-27 04:15:04 +0700

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -1,80 +1,83 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 st.write("""
 # Penguin Prediction App
-
 This app predicts the **Palmer Penguin** species!
-
 Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
 
-st.sidebar.header('User Input Features')
+img = Image.open('lter_penguins.png')
+img = img.resize((700, 418))
+st.image(img, use_column_width=False)
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
+img2 = Image.open('culmen_depth.png')
+img2 = img2.resize((700, 451))
+st.image(img2, use_column_width=False)
 
-# Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
+st.sidebar.header('Parameter Inputan')
+
+# Upload File CSV untuk parameter inputan
+uplouad_file = st.sidebar.file_uploader("Upload file CSV Anda", type=["csv"])
+if uplouad_file is not None:
+    inputan = pd.read_csv(uplouad_file)
 else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
+    def input_user():
+        pulau = st.sidebar.selectbox('Pulau', ('Biscoe', 'Dream', 'Torgersen'))
+        gender = st.sidebar.selectbox('Gender',('Pria', 'Wanita'))
+        panjang_paruh = st.sidebar.selectbox('Panjang Paruh (mm)', 32.1,59.6,43.9)
+        kedalaman_paruh = st.sidebar.selectbox('Kedalaman Paruh (mm)',13.1,21.5,17.2)
+        panjang_sirip = st.sidebar.selectbox('Panjang Sirip (mm)',172,0,231.0,201.0)
+        masa_tubuh = st.sidebar.selectbox('Masa Tubuh (g)',2700.0,6300.0,4207.0)
+        data = {'pulau' : pulau,
+                'panjang_paruh_mm' : panjang_paruh,
+                'kedalaman_paruh_mm' : kedalaman_paruh,
+                'panjang_sirip_mm' : panjang_sirip,
+                'masa_tubuh_g' : masa_tubuh,
+                'gender' : gender}
+        fitur = pd.DataFrame(data, index=[0])
+        return fitur
+    inputan = input_user()
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'])
-df = pd.concat([input_df,penguins],axis=0)
+# Menggabungkan inputan data dataset penguin
+penguin_raw = pd.read_csv('penguins_example.csv')
+penguins = penguin_raw.drop(columns=['jenis'])
+df = pd.concat([inputan, penguins], axis=0)
 
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
+# Encode untuk fitur ordinal
+encode = ['gender', 'pulau']
 for col in encode:
     dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
+    df = pd.concat([df, dummy], axis=1)
     del df[col]
-df = df[:1] # Selects only the first row (the user input data)
+df = df[:1] #ambil barisan pertama (input dan user)
 
-# Displays the user input features
-st.subheader('User Input features')
+# Menampilkan parameter hasil inputan
+st.subheader('parameter Inputan')
 
-if uploaded_file is not None:
+if uplouad_file is not None:
     st.write(df)
 else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write('Menunggu file csv untuk di upload. saat ini memakai sampel inputan(seperti tampilan di bawah).')
     st.write(df)
 
-# Reads in saved classification model
-load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+#
+load_model = pickle.load(open('penguins_clf.pkl','rb'))
 
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
+#Terapan model 
+prediksi = load_model.predict(df)
+prediksi_proba = load_model.predict_proba(df)
 
+st.subheader('Keterangan Label Kelas')
+jenis_penguin = np.array(['Adelie','Chinstrap','Gento'])
+st.write(jenis_penguin)
 
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
+st.subheader('Hasil perediksi (Klasifikasi penguin)')
+st.write(jenis_penguin[prediksi])
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+st.subheader('Probalitas Hasil Prediksi (kalsifikasi penguin)')
+st.write(prediksi_proba)
